4421381491b24dce2e893be97929b755ba313e59
DUCLUSAUD Marc <marc.duclusaud@bordeaux-inp.fr>
2022-01-12 19:40:28 +0100

    URDF with angles

--- Project/simulation.py
+++ Project/simulation.py
@@ -4,19 +4,13 @@ import pybullet as p
 import pybullet_data
 
 class OpenDog:
-    """
-    Links : self.legs[leg_id][i] = id du i-ème membre de la patte leg_id
-    Joints : self.joint_name_to_id['ij'] = id du j-ème joint de la patte i
-    """
 
     def __init__(
             self,
     ):
-        self.id = p.loadURDF("urdf/basic_robot.urdf", [0, 0, 0.6], p.getQuaternionFromEuler([0, 0, 0]))
-        # self.connectors = [[16, 8, 6, 9, 12, 17, 15, 72],
-        #                    [34, 26, 24, 27, 30, 35, 33, 73],
-        #                    [52, 44, 42, 45, 48, 53, 51, 74],
-        #                    [70, 62, 60, 63, 66, 71, 69, 75]]
+
+        self.id = p.loadURDF("urdf/opendog.urdf", [0, 0, 0.6], p.getQuaternionFromEuler([0, 0, 0]))
+        self.legs = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11]]
         self._buildJointNameToIdDict()
 
     def _buildJointNameToIdDict(self):
@@ -54,38 +48,36 @@ class OpenDog:
     #                             controlMode=p.POSITION_CONTROL, force=force)
     #     p.setJointMotorControl2(self.id, self.joint_name_to_id['3v3'], targetPosition=0.4455 - V[11] * 0.001,
     #                             controlMode=p.POSITION_CONTROL, force=force)
-    #
-    # def getCenterOfMassSimu(self):
-    #     """
-    #     Calcule les coordonnées du centre de masse du Megabot simulé à partir de l'URDF
-    #     :return: coordonnées du centre de masse
-    #     """
-    #     nb_links = p.getNumJoints(self.id)
-    #     base_pos = p.getBasePositionAndOrientation(self.id)[0]
-    #     base_mass = p.getDynamicsInfo(self.id, -1)[0]
-    #     com = [base_pos[0] * base_mass, base_pos[1] * base_mass, base_pos[2] * base_mass]
-    #     mass = base_mass
-    #     for i in range(nb_links):
-    #         link_pos = p.getLinkState(self.id, i)[0]
-    #         link_mass = p.getDynamicsInfo(self.id, i)[0]
-    #         com = [com[0] + link_pos[0] * link_mass, com[1] + link_pos[1] * link_mass, com[2] + link_pos[2] * link_mass]
-    #         mass += link_mass
-    #     return [com[0] / mass, com[1] / mass, com[2] / mass]
 
-    # def DrawCOM(self):
-    #     p.addUserDebugLine(pl.get_leg_pos())
+    def getCOM(self):
+        """
+        Calcule les coordonnées du centre de masse de l'OpenDog
 
+        :return: coordonnées du centre de masse
+        """
+        nb_links = p.getNumJoints(self.id)
+        base_pos = p.getBasePositionAndOrientation(self.id)[0]
+        base_mass = p.getDynamicsInfo(self.id, -1)[0]
+        com = [base_pos[0] * base_mass, base_pos[1] * base_mass, base_pos[2] * base_mass]
+        mass = base_mass
+        for i in range(nb_links):
+            link_pos = p.getLinkState(self.id, i)[0]
+            link_mass = p.getDynamicsInfo(self.id, i)[0]
+            com = [com[0] + link_pos[0] * link_mass, com[1] + link_pos[1] * link_mass, com[2] + link_pos[2] * link_mass]
+            mass += link_mass
+        return [com[0] / mass, com[1] / mass, com[2] / mass]
 
-# def draw(pt, color=[1, 0, 0], durationTime=0):
-#     """
-#     Trace un point lors de la simulation
-#     :param pt: coordonnées du point
-#     :param color: couleur du point
-#     :param durationTime: durée en secondes d'affichage du point
-#     :return: None
-#     """
-#     end_pt = [pt[0]+0.0025, pt[1], pt[2]]
-#     p.addUserDebugLine(pt, end_pt, lineColorRGB=color, lineWidth=10, lifeTime=durationTime)
+def draw(pt, color=[1, 0, 0], durationTime=0):
+    """
+    Trace un point lors de la simulation
+
+    :param pt: coordonnées du point
+    :param color: couleur du point
+    :param durationTime: durée en secondes d'affichage du point
+    :return: None
+    """
+    end_pt = [pt[0]+0.0025, pt[1], pt[2]]
+    p.addUserDebugLine(pt, end_pt, lineColorRGB=color, lineWidth=10, lifeTime=durationTime)
 
 
 ################################ SIMULATION ################################
@@ -99,11 +91,29 @@ opendog = OpenDog()
 FRAME_RATE = 1 / 10 # Fréquence à laquelle un ordre est transmis au vérins
 TICK_RATE = 1 / 240 # Fréquence à laquelle le simulateur s'actualise
 
-for i in range(p.getNumJoints(opendog.id)):
-    p.setDebugObjectColor(opendog.id, i, [1., 0., 0.])
+# p.changeVisualShape(opendog.id, -1, rgbaColor=[1, 1, 1, 1])
+p.changeVisualShape(opendog.id, 0, rgbaColor=[1, 0, 0, 1])
+p.changeVisualShape(opendog.id, 1, rgbaColor=[1, 0, 0, 1])
+p.changeVisualShape(opendog.id, 2, rgbaColor=[1, 0, 0, 1])
+p.changeVisualShape(opendog.id, 3, rgbaColor=[1, 1, 0, 1])
+p.changeVisualShape(opendog.id, 4, rgbaColor=[1, 1, 0, 1])
+p.changeVisualShape(opendog.id, 5, rgbaColor=[1, 1, 0, 1])
+p.changeVisualShape(opendog.id, 6, rgbaColor=[0, 1, 0, 1])
+p.changeVisualShape(opendog.id, 7, rgbaColor=[0, 1, 0, 1])
+p.changeVisualShape(opendog.id, 8, rgbaColor=[0, 1, 0, 1])
+p.changeVisualShape(opendog.id, 9, rgbaColor=[0, 0, 1, 1])
+p.changeVisualShape(opendog.id, 10, rgbaColor=[0, 0, 1, 1])
+p.changeVisualShape(opendog.id, 11, rgbaColor=[0, 0, 1, 1])
+
 
 for i in range(10000):
     p.stepSimulation()
+
+    # for i in range(p.getNumJoints(opendog.id)):
+    #     print(i, ":", p.getLinkState(opendog.id, i)[0])
+
+    draw(opendog.getCOM())
+
     time.sleep(TICK_RATE)
 
 p.disconnect()
