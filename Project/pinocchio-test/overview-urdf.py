156efe71abb2b0be7fba96baea367fbf1adcb1f0
motheguy <motheguy@ensc.fr>
2022-01-14 14:34:05 +0100

    beginning pinocchio -kinematics tree

---
+++ Project/pinocchio-test/overview-urdf.py
@@ -0,0 +1,26 @@
+import pinocchio
+from sys import argv
+from os.path import dirname, join, abspath
+
+pinocchio_model_dir = dirname(dirname(str(abspath(__file__))))
+
+urdf_filename = pinocchio_model_dir + '/urdf/robot.urdf' if len(argv)<2 else argv[1]
+
+# Load the urdf model
+model = pinocchio.buildModelFromUrdf(urdf_filename)
+# print('model name: ' + model.name)
+
+# Create data required by the algorithms
+data = model.createData()
+
+# Sample a random configuration
+q = pinocchio.randomConfiguration(model)
+print('q: %s' % q.T)
+
+# Perform the forward kinematics over the kinematic tree
+pinocchio.forwardKinematics(model,data,q)
+ 
+# Print out the placement of each joint of the kinematic tree
+for name, oMi in zip(model.names, data.oMi):
+    print(("{:<24} : {: .2f} {: .2f} {: .2f}"
+          .format( name, *oMi.translation.T.flat )))
